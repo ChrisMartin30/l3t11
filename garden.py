661e8230cf3f7ae021802f6e8db55b81cb72be25
Chris Martin <martin.chris.30@live.com>
2023-05-17 21:42:39 +0100

    Start of task

---
+++ garden.py
@@ -0,0 +1,11 @@
+# L3T11 - Compulsory Task 1
+
+import spacy
+
+# Create a list containing garden path sentences
+
+gardenpathSentences = ["We painted the wall with cracks.", 
+                       "The man who hunts ducks out on weekends.",
+                       "Mary gave the child a Band-Aid.",
+                       "That Jill is never here hurts.",
+                       "The cotton clothing is made of grows in Mississippi."]
